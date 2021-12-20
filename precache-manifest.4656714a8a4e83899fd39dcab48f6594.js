6d0cbe99579c0b9f4fab618c9cde5b68bad347d8
JC Navarro <kal-jc@hotmail.com>
2021-12-20 13:56:54 -0800

    Updating date functionality

---
+++ precache-manifest.4656714a8a4e83899fd39dcab48f6594.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302d6c99eb7ac2c1ccfdf6c54cfc28f7",
+    "url": "/rp-medical-clinic/index.html"
+  },
+  {
+    "revision": "012d14d6a50d8495622a",
+    "url": "/rp-medical-clinic/static/css/2.2cb57e9d.chunk.css"
+  },
+  {
+    "revision": "3499cf5a5c42187e10d7",
+    "url": "/rp-medical-clinic/static/css/main.58ba2a1c.chunk.css"
+  },
+  {
+    "revision": "012d14d6a50d8495622a",
+    "url": "/rp-medical-clinic/static/js/2.c3ea1589.chunk.js"
+  },
+  {
+    "revision": "4d1006dbdac151d7598a1a35010e25e6",
+    "url": "/rp-medical-clinic/static/js/2.c3ea1589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3499cf5a5c42187e10d7",
+    "url": "/rp-medical-clinic/static/js/main.16871269.chunk.js"
+  },
+  {
+    "revision": "fb413f1bd261421c883b",
+    "url": "/rp-medical-clinic/static/js/runtime-main.72b0cf4c.js"
+  },
+  {
+    "revision": "0cea484121d47a69d806a54957b886d1",
+    "url": "/rp-medical-clinic/static/media/1.0cea4841.png"
+  },
+  {
+    "revision": "84f2c4c2416e548be33544f3f53988ca",
+    "url": "/rp-medical-clinic/static/media/1.84f2c4c2.jpg"
+  },
+  {
+    "revision": "5341e7014ca2b19ffa26e4f84c4d4fae",
+    "url": "/rp-medical-clinic/static/media/2.5341e701.jpg"
+  },
+  {
+    "revision": "92837a203f0eb773631f579ddd426543",
+    "url": "/rp-medical-clinic/static/media/Oswald-Bold.92837a20.ttf"
+  },
+  {
+    "revision": "e605f2f859072ed0443aeda5adec4181",
+    "url": "/rp-medical-clinic/static/media/Oswald-Regular.e605f2f8.ttf"
+  },
+  {
+    "revision": "15d4a71bc47a16a11dbc9973f85c7413",
+    "url": "/rp-medical-clinic/static/media/RPMC (10).15d4a71b.jpg"
+  },
+  {
+    "revision": "fe33253b867865a2b2cca4282b5a4d97",
+    "url": "/rp-medical-clinic/static/media/RPMC (11).fe33253b.jpg"
+  },
+  {
+    "revision": "0bfb7b1add443121155230ce9ec2c82b",
+    "url": "/rp-medical-clinic/static/media/RPMC (12).0bfb7b1a.jpg"
+  },
+  {
+    "revision": "252f2f3186a9aad515fbbf22b4170925",
+    "url": "/rp-medical-clinic/static/media/RPMC (13).252f2f31.jpg"
+  },
+  {
+    "revision": "5061d95899be8cda252757297111e322",
+    "url": "/rp-medical-clinic/static/media/RPMC (14).5061d958.jpg"
+  },
+  {
+    "revision": "95f1e36c303b6eefffff99c7bd67a33f",
+    "url": "/rp-medical-clinic/static/media/RPMC (15).95f1e36c.jpg"
+  },
+  {
+    "revision": "0aec31c270bd4b5009882ad2471cf21b",
+    "url": "/rp-medical-clinic/static/media/RPMC (17).0aec31c2.jpg"
+  },
+  {
+    "revision": "5341e7014ca2b19ffa26e4f84c4d4fae",
+    "url": "/rp-medical-clinic/static/media/RPMC (18).5341e701.jpg"
+  },
+  {
+    "revision": "de3f10866b0cc1d77ffb32aa7cd50b74",
+    "url": "/rp-medical-clinic/static/media/RPMC (19).de3f1086.jpg"
+  },
+  {
+    "revision": "b013c74b6a68b5a9c7b6caf65709b2f8",
+    "url": "/rp-medical-clinic/static/media/RPMC (2).b013c74b.jpg"
+  },
+  {
+    "revision": "e68ad69479479d522be52d4881c8b92a",
+    "url": "/rp-medical-clinic/static/media/RPMC (20).e68ad694.jpg"
+  },
+  {
+    "revision": "77476ade1f41010d0898bfdffb978551",
+    "url": "/rp-medical-clinic/static/media/RPMC (21).77476ade.jpg"
+  },
+  {
+    "revision": "02a062b835aa8d7cf0f8b55b8f43d681",
+    "url": "/rp-medical-clinic/static/media/RPMC (22).02a062b8.jpg"
+  },
+  {
+    "revision": "d688a2aeb10835c89061414e06599ae8",
+    "url": "/rp-medical-clinic/static/media/RPMC (23).d688a2ae.jpg"
+  },
+  {
+    "revision": "d22eb63d2598510d0faa6baca4c45bc4",
+    "url": "/rp-medical-clinic/static/media/RPMC (24).d22eb63d.jpg"
+  },
+  {
+    "revision": "f787339a68393c30f0aa0dfb5590e9a7",
+    "url": "/rp-medical-clinic/static/media/RPMC (3).f787339a.jpg"
+  },
+  {
+    "revision": "894a67af1d4f014a065987488f44fd09",
+    "url": "/rp-medical-clinic/static/media/RPMC (4).894a67af.jpg"
+  },
+  {
+    "revision": "9746937c6d05650af213fbe6e46d0888",
+    "url": "/rp-medical-clinic/static/media/RPMC (5).9746937c.jpg"
+  },
+  {
+    "revision": "976b0712e670d7c9a5daeb71bccc152e",
+    "url": "/rp-medical-clinic/static/media/RPMC (6).976b0712.jpg"
+  },
+  {
+    "revision": "22aa6ca698c709bfd0095cd9b800474e",
+    "url": "/rp-medical-clinic/static/media/RPMC (7).22aa6ca6.jpg"
+  },
+  {
+    "revision": "5bb84c2857c7b6d326c30e9871e1a5d5",
+    "url": "/rp-medical-clinic/static/media/RPMC (8).5bb84c28.jpg"
+  },
+  {
+    "revision": "24ece394a93533c9b159c962780b45e0",
+    "url": "/rp-medical-clinic/static/media/RPMC (9).24ece394.jpg"
+  },
+  {
+    "revision": "f905e96da2f85dd60f13b550663cdb07",
+    "url": "/rp-medical-clinic/static/media/RPMC.f905e96d.jpg"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/rp-medical-clinic/static/media/getFetch.51c934af.cjs"
+  },
+  {
+    "revision": "8b8ee9d135bd189154130e595149f5de",
+    "url": "/rp-medical-clinic/static/media/home.8b8ee9d1.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/rp-medical-clinic/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/rp-medical-clinic/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/rp-medical-clinic/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/rp-medical-clinic/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8890fd748812ae30e31b980956d2b3c.js"
+  "/rp-medical-clinic/precache-manifest.4656714a8a4e83899fd39dcab48f6594.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rp-medical-clinic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
