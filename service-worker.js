3edfb1fbb90e8b5b02a6006a5952dbad2224e946
JC Navarro <kal-jc@hotmail.com>
2023-07-10 17:53:27 -0700

    Updating dates and hiding testimonials

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.813803dce35b49a8b14a10cad9064866.js"
+  "./precache-manifest.f2617be90019f87c5ec2c4e007df8428.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
